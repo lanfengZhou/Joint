23e8138509c27cf0eecde09a1399be19a6fc7298
lanfengZhou <yihuoZhou@163.com>
2017-06-20 15:29:13 +0800

    2017/6/20 editsql byzhou

--- lib/db/manage_datas.sql
+++ lib/db/manage_datas.sql
@@ -10,10 +10,37 @@ Target Server Type    : MYSQL
 Target Server Version : 50610
 File Encoding         : 65001
 
-Date: 2017-06-06 18:00:53
+Date: 2017-06-20 15:28:33
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for device
+-- ----------------------------
+DROP TABLE IF EXISTS `device`;
+CREATE TABLE `device` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `alias` varchar(80) NOT NULL,
+  `number` varchar(40) NOT NULL,
+  `belong` varchar(20) NOT NULL,
+  `info` varchar(80) DEFAULT NULL,
+  `state` int(2) NOT NULL,
+  `insertTime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of device
+-- ----------------------------
+INSERT INTO `device` VALUES ('1', '实物1', '1', 'A', null, '1', '2017-06-20 14:04:26');
+INSERT INTO `device` VALUES ('2', '实物2', '2', 'A', null, '1', '2017-06-20 14:04:50');
+INSERT INTO `device` VALUES ('3', '实物3', '3', 'A', null, '1', '2017-06-20 14:05:20');
+INSERT INTO `device` VALUES ('4', '实物4', '4', 'A', null, '1', '2017-06-20 14:05:37');
+INSERT INTO `device` VALUES ('5', '实物5', '5', 'A', null, '1', '2017-06-20 14:05:55');
+INSERT INTO `device` VALUES ('6', '实物6', '6', 'A', null, '1', '2017-06-20 14:06:13');
+INSERT INTO `device` VALUES ('7', '实物7', '7', 'A', null, '1', '2017-06-20 14:07:02');
+INSERT INTO `device` VALUES ('9', '实物8', '8', 'A', '2222', '1', '2017-06-20 15:18:20');
 
 -- ----------------------------
 -- Table structure for user
